8dd9bcddd023263a3cd1370ba1507461f6cb1008
Zelda Lipschutz <68125655+zlip123@users.noreply.github.com>
2023-10-15 02:06:28 -0400

    Update Server.js

--- Server.js
+++ Server.js
@@ -9,25 +9,38 @@ app.use(bodyParser.text());
 
 app.post('/runcode', (req, res) => {
     const code = req.body;
+    const selectedLanguage = 'python'; // Default language is Python
 
-    // Run Python code using child_process
-    exec('python -c "' + code + '"', (error, stdout, stderr) => {
+    let command = '';
+    switch (selectedLanguage) {
+        case 'python':
+            command = `python -c "${code}"`;
+            break;
+        case 'java':
+            command = `java -Xmx256m -Xss512k -cp . Main`;
+            break;
+        case 'css':
+            // Implement CSS execution logic here (if needed)
+            break;
+        default:
+            res.status(400).send('Invalid language selection');
+            return;
+    }
+
+    exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error: ${error}`);
             res.status(500).send(error);
         } else {
             console.log(`Output: ${stdout}`);
             console.error(`Error Output: ${stderr}`);
-
-            // Send the output as the response
             res.send(stdout);
         }
     });
 });
 
+app.use(express.static('public'));
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-// Client-side JavaScript
-app.use(express.static('public'));
